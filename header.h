5dadce1b0cc9e1076a0dafe42d79efe81e92878f
Emanuele <55718523+JutJ3di@users.noreply.github.com>
2021-01-06 20:17:51 +0100

    Create header.h

---
+++ header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER
+#define HEADER
+
+#define SIZEOF(x) sizeof(Mex) - sizeof(long)
+
+#define EXIT 10
+
+#define PATH ".."
+#define KEY  'A'
+
+typedef struct{
+
+    long tipo;
+
+    char s;
+
+}Mex;
+
+#endif
